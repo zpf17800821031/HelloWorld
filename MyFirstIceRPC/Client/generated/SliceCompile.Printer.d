80779cdceee82c7042bf22f83b96dfcf96a7023a
zpf17800821031@outlook.com <zpf17800821031@outlook.com>
2024-10-08 16:12:55 +0800

    Ice RPC doc for Hello World Application: https://doc.zeroc.com/ice/latest/hello-world-application

---
+++ MyFirstIceRPC/Client/generated/SliceCompile.Printer.d
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<dependencies>
+  <source name="Printer.ice">
+    <options>
+      <IceHome>C:\Users\zpf17\.nuget\packages\zeroc.ice.net\3.7.10\build\..</IceHome>
+      <IceToolsPath>C:\Users\zpf17\.nuget\packages\zeroc.ice.net\3.7.10\build\..\tools</IceToolsPath>
+      <OutputDir>C:\Users\zpf17\source\repos\zpf17800821031\HelloWorld\MyFirstIceRPC\Client\generated</OutputDir>
+    </options>
+  </source>
+</dependencies>
